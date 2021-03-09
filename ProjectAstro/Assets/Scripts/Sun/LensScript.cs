c8c9d3f6201c18692d122567efc564785ec40b6e
Adrien J <ajeanne25@gmail.com>
2021-03-09 17:32:39 +0100

    sun earth and moon

---
+++ ProjectAstro/Assets/Scripts/Sun/LensScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LensScript : MonoBehaviour
+{
+
+    public LensFlare lensFlare;
+	public float strength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        strength = 3500f;
+        lensFlare = GetComponent<LensFlare>();
+		Vector3 heading = gameObject.transform.position - Camera.main.transform.position;
+		float dist = heading.magnitude;
+        lensFlare.brightness = strength / dist;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 heading = gameObject.transform.position - Camera.main.transform.position;
+		float dist = heading.magnitude;
+        lensFlare.brightness = Mathf.Clamp(strength / dist, 1, Mathf.Infinity);
+    }
+}
